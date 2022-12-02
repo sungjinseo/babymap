8d4e0a4b91af24f57540b1a7b351fd3914c95091
greatSeo <sjseo85@gmail.com>
2022-12-02 16:11:15 +0900

    spring init

---
+++ src/main/java/dev/greatseo/babymap/BabymapApplication.java
@@ -0,0 +1,13 @@
+package dev.greatseo.babymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabymapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabymapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/greatseo/babymap/BabymapApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.greatseo.babymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabymapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
